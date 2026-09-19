33847993db6f36ffffb2033200bacf4fe2c8a64e
abishaa0808-netizen <abishaa0808@gmail.com>
2026-09-19 20:57:42 +0530

    backend

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.get("/" , (req, res) => {
+    res.send("Multilingual Government Scheme Assistant Backend is running.");
+});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
