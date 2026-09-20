e5989d234c5208380fc3b5a865b53c78ba7900cb
abishaa0808-netizen <abishaa0808@gmail.com>
2026-09-20 19:27:07 +0530

    backend

--- agent.js
+++ agent.js
@@ -1,9 +1,12 @@
-function runAgent(question, language)
-{
+const tools = require("./tools");
+
+function runAgent(question, language) {
     return {
         question: question,
         language: language,
-        message: "Agent is ready."
+        message: "Agent is ready to search schemes.",
+        toolsAvailable: Object.keys(tools)
     };
 }
+
 module.exports = runAgent;
