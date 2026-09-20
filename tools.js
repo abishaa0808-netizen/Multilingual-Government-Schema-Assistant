f1f1712a50d5195ffadba4187bc752d07f3ce2a4
abishaa0808-netizen <abishaa0808@gmail.com>
2026-09-20 18:40:49 +0530

    back

---
+++ tools.js
@@ -0,0 +1,9 @@
+function searchSchemes(schemes, keyword) {
+    return schemes.filter(scheme =>
+        scheme.name.toLowerCase().includes(keyword.toLowerCase()) ||
+        scheme.sector.toLowerCase().includes(keyword.toLowerCase())
+    );
+}
+module.exports = {
+        searchSchemes
+};
